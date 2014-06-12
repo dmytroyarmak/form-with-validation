ac7724fcbe898f44aeb0b57fb3e70db58ff96694
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-06-12 09:00:14 +0300

    Simple form validation

--- js/main.js
+++ js/main.js
@@ -37,3 +37,81 @@ $(function() {
     configAllPlaceholders();
   }
 });
+
+$(function() {
+  var $form = $('js-register-form form');
+
+  var $successAjax = function(options) {
+    if (options.success) {
+      setTimeout(function ajaxSuccessCallback() {
+        options.success.call(options.context);
+      }, 200);
+    }
+  };
+
+  var $errorAjax = function(options) {
+    if (options.error) {
+      setTimeout(function ajaxErrorCallback() {
+        options.error.call(options.context, {
+          responseJSON: {
+            errors: {
+              first_name: 'Это поле обязательное',
+              password: 'Слишком простой пароль',
+              email: 'Неверный формат email '
+            }
+          }
+        });
+      }, 200);
+    }
+  };
+
+  var showValidationsErrors = function(errors) {
+    $.each(errors, function(field, error) {
+      var $errorMsg = $('<span>').addClass('help-block').text(error);
+
+      $('#' + field)
+        .after($errorMsg)
+        .closest('.form-group').addClass('has-error');
+    });
+  };
+
+  var hideValidationsErrors = function() {
+    $('.form-group.has-error').removeClass('has-error');
+    $('.help-block').remove();
+  };
+
+  $('.js-btn-success').on('click', function(e) {
+    e.preventDefault();
+
+    hideValidationsErrors();
+
+    $successAjax({
+      url : '/register',
+      method: 'POST',
+      dataType: 'json',
+      data: $form.serializeArray(),
+      success: function onSubmitSuccess() {
+        $('.js-register-form').hide();
+        $('.js-register-thank-you').show();
+      }
+    });
+  });
+
+  $('.js-btn-error').on('click', function(e) {
+    e.preventDefault();
+
+    hideValidationsErrors();
+
+    $errorAjax({
+      url : '/register',
+      method: 'POST',
+      dataType: 'json',
+      data: $form.serializeArray(),
+      error: function onSubmitError(data) {
+        if (data.responseJSON && data.responseJSON.errors) {
+          showValidationsErrors(data.responseJSON.errors);
+        }
+      }
+    });
+  });
+});
